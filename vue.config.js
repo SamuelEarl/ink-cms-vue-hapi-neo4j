faeb89d9805c673a6ce1388ee36be001f19be362
Sam Earl <samueltremain@gmail.com>
2019-05-22 18:48:05 -0500

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    entry: path.join(__dirname, "src/client/main.js"),
+    devtool: "source-map",
+  },
+  outputDir: path.join(__dirname, "dist/client"),
+
+  devServer: {
+    proxy: "http://localhost:3000"
+  },
+
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type => addStyleResource(config.module.rule("stylus").oneOf(type)));
+  },
+};
+
+function addStyleResource (rule) {
+  rule.use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [
+        path.resolve(__dirname, "./src/client/styles/main.styl"),
+      ],
+    });
+}
