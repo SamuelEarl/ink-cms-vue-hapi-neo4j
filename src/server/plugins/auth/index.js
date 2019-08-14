11e95c17ac3efd7574c4f95e701e7bc7fab1338d
Sam Earl <samueltremain@gmail.com>
2019-08-14 14:37:06 -0500

    Added some notes for a server method that I want to implement

--- src/server/plugins/auth/index.js
+++ src/server/plugins/auth/index.js
@@ -14,5 +14,35 @@ exports.plugin = {
     // Set session to the Neo4j "session" database object
     const session = server.app.session;
 
+    // // Create a "helpers" plugin that has a server method called "catch", like this:
+    // function(e, message, path) {
+    //   let err;
+
+    //   if (message) {
+    //     err = new Boom(message);
+    //   }
+    //   else {
+    //     err = new Boom(e);
+    //   }
+
+    //   // `path` is from `request.path`
+    //   const errorLog = `[ENDPOINT]: ${path}\n[ERROR]: ${err}`;
+
+    //   // Log the error:
+    //   console.error(errorLog);
+    //   // The nice thing about using a helper method is that you could add logging here and it would be added for every catch block in every route.
+
+    //   return err;
+    // }
+
+    // // Call the "catch" method:
+    // catch(e) {
+    //   const msg = "Some error message";
+    //   const errorRes = server.methods.catch(e, msg, request.path);
+    //   error = errorRes;
+    // }
+
+    // // Also, do I need to go back to any of the places where I used "throw new Error()" and replace those with a more descriptive "throw new Boom('msg', { statusCode: 400})"?
+    // // Right now everything throws a 500 error, but that might not be the most accurate error message to send back to the user.
   }
 };
